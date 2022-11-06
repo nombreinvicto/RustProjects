8fb4dbd1b3a16758206c93a8c0f89baf2def333e
nombreinvicto <mahmud.buet.me@gmail.com>
2022-11-05 18:24:52 -0600

    added exercise_f for structs and traits

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,9 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let one_second = time::Duration::from_secs(1);
+    loop {
+        println!("Hello, world!");
+        thread::sleep(one_second);
+    }
 }
